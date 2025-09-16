467c2092c530394029dfdaec1cc3801dcafdd60c
Runner-st <m36369682@gmail.com>
2025-09-16 17:15:18 +0300

    Added CustoOverride file

---
+++ custom/cmake/CustomOverrides.cmake
@@ -0,0 +1,16 @@
+# C++ файли плагіна
+list(APPEND CUSTOM_SOURCES
+    ${CMAKE_SOURCE_DIR}/custom/src/CustomPlugin.cc
+    ${CMAKE_SOURCE_DIR}/custom/src/CustomPlugin.h
+)
+
+# QML файл плагіна
+set(QGC_CUSTOM_QML
+    ${CMAKE_SOURCE_DIR}/custom/src/FlyViewCustomLayer.qml
+    CACHE INTERNAL "Custom QML files"
+)
+
+# Include для заголовків
+list(APPEND CUSTOM_INCLUDE_DIRECTORIES
+    ${CMAKE_SOURCE_DIR}/custom/src
+)
