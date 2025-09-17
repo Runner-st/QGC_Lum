5dda158fcc3222ac8f34177ede61b15adb834ea1
Runner-st <m36369682@gmail.com>
2025-09-17 20:50:21 +0300

    Add custom version override for Lumiere build

---
+++ custom/src/QGroundControlQmlGlobalCustom.cc
@@ -0,0 +1,25 @@
+/****************************************************************************
+ *
+ * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#include "QGroundControlQmlGlobal.h"
+
+#include <QtCore/QSysInfo>
+
+QString QGroundControlQmlGlobal::qgcVersion()
+{
+    QString versionStr = QStringLiteral("Lumiere QGC v0.1");
+
+    if (QSysInfo::buildAbi().contains("32")) {
+        versionStr += QStringLiteral(" %1").arg(tr("32 bit"));
+    } else if (QSysInfo::buildAbi().contains("64")) {
+        versionStr += QStringLiteral(" %1").arg(tr("64 bit"));
+    }
+
+    return versionStr;
+}
